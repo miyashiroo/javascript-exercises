caf4b100724a052ee93b363b9ffafefd88bb5350
miyashiroo <miyashiroo@gmail.com>
2021-08-30 21:08:50 -0400

    fundamentals 4 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const ftoc = function() {
+// ftoc(32) // fahrenheit to celsius, should return 0
 
+// ctof(0) // celsius to fahrenheit, should return 32
+const ftoc = function(fahrenheit) {
+  const temperatureInFahrenheit = fahrenheit
+  const temperatureInCelcius = (temperatureInFahrenheit - 32) /  1.8
+  const fixedCelcius = temperatureInCelcius.toFixed(1)
+
+  return parseFloat(fixedCelcius)
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  const temperatureInCelcius = celcius
+  const temperatureInFahrenheit = (temperatureInCelcius * 1.8) + 32
+  const fixedFahrenheit = temperatureInFahrenheit.toFixed(1)
+  return parseFloat(fixedFahrenheit)
 
 };
 
